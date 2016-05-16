4d0a293abae290dc2ace438bb947273f58bd1670
pranati05 <pranati05@gmail.com>
2016-05-16 15:23:26 -0700

    Add files via upload

--- js/myjs.js
+++ js/myjs.js
@@ -2,56 +2,83 @@
 // Display Popup
 
 $(document).ready(function() {
- $('#phone').hide();
- 
- 
-    
-   
 
-$('.links').click(function(e)
-{
-     e.preventDefault();
-   $('contactForm').validate();
-    
-    $('#content').replaceWith($('.page2').html());
-	  
 
+
+$(document).on('click','.links',function(e){
+	e.preventDefault();
 	
-   $('#state select').change(function() {
+    $('#content').load('prechat.html');
+	
+   // $('.content').hide();
+   // $('.content').replaceWith($('.page2').html());
+	  
+	
+
+   });
+$(document).on('change','#state select',function(e){
+  e.preventDefault();
 
 var value= $(this).val();
   
- if(value == 'CA') { 
- $('#phone').show(); 
+ if(value == 'CA') { //if this value is NOT selected
+ $('#phone').show(); //this field is shown
 	 } 
  else {
- $('#phone').hide();
+ $('#phone').hide();//else it is removed
+ 
 	 }
     return false;
  
 	  });
 	  
+$(document).on('click','#submit',function(e){
+     $($('form[name="form1"]')).validate({ // initialize the plugin
+        rules: {
+            fullname: {
+                required: true
+                
+            },
+            mobilenumber: {
+                required: true,
+              	phoneUS: true
+            }
+        },
+		highlight: function(element) {
+        var id_attr = "#" + $( element ).attr("id") + "1";
+        $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+        $(id_attr).addClass('glyphicon-alert');         
+    },
+    unhighlight: function(element) {
+        var id_attr = "#" + $( element ).attr("id") + "1";
+        $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+        $(id_attr).removeClass('glyphicon-alert');         
+    },
+    errorElement: 'span',
+        errorClass: 'help-block',
+        errorPlacement: function(error, element) {
+            if(element.length) {
+                error.insertAfter(element);
+            } else {
+            error.insertAfter(element);
+            }
+        },
+        submitHandler: function (form) { // for demo
+             $('#page2').replaceWith('<p style="padding-left:"25px">Congrats! Form has been submitted successfully</p>').html();
+		
+            return false; // for demo
+        }
+    });
+});
 
-$('#submit').click(function(e)
-{
-      
-       $( "#container" ).html( "<p class='page3'>Congratulations!Message was submitted successfully.</p>" );
-      
-       
-        
-        
-   });
-
-
-
+ });
 
    
-}); 
-	  
+
 	  
 
 
-});
+
  
 
    
